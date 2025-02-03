11bdbeb2f421b93902a935ba21aed082ba0243e9
張雁舒 <77557596+mm-news@users.noreply.github.com>
2025-02-03 07:41:45 +0000

    Rename the `get_field_value` function

--- functions/random.py
+++ functions/random.py
@@ -1,11 +1,14 @@
+from functions.field_control import Field
+
+
 def get_random_rc(size):
     from random import randint
     return randint(0, size - 1), randint(0, size - 1)
 
 
-def get_random_rc_of_empty_field(size, field):
+def get_random_rc_of_empty_field(size: int, field: Field):
     rc = get_random_rc(size)
-    if field.get_field_value(rc[0], rc[1]) == 0:
+    if field.get_by_rc(rc[0], rc[1]) == 0:
         return rc
     else:
         return get_random_rc_of_empty_field(size, field)
